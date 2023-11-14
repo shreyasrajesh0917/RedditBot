35b050021d753e840f0f67af2c50de11dac9a331
Shreyas Rajesh <89420948+shreyasrajesh0917@users.noreply.github.com>
2023-11-13 22:19:46 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-username = Shoddy-Actuator1074
-password = Yashy2005
-clientId = 2No6v3HahQSbhLEsicZTuQ
-secretId = ajhSOTqTGXDkx5NZEapMq7NtHOXs9g
+username = #insert custom username
+password = #insert custom password
+clientId = #insert custom Client ID
+secretId = #insert custom Secret ID
